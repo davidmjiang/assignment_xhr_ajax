ffe139b330c29c25710f70367fe4272bfe66a514
David Jiang <davidjgrey@gmail>
2016-09-16 10:18:34 -0700

    created jquery get

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-$(document).ready(function() {
-  var xhr = new XMLHttpRequest();
-  xhr.addEventListener( "load", function(){
-      console.log( this );
-  });
-  xhr.open("GET", "http://reqres.in/api/users", true);
-  xhr.send();
+  // var xhr = new XMLHttpRequest();
+  // xhr.addEventListener( "load", function(){
+  //     console.log( this );
+  // });
+  // xhr.open("GET", "http://reqres.in/api/users", true);
+  // xhr.send();
 
 
-  // Create a post
-  var xhr = new XMLHttpRequest();
-  xhr.addEventListener( "load", function(){
-      console.log( this );
-  });
-  xhr.addEventListener( "error", function(){
-      console.log( this );
-  });
-  xhr.open("POST", "http://reqres.in/api/posts", true);
-  xhr.send("title=Foo&body=Bar&userId=100000000000000");
-})
+  // // Create a post
+  // var xhr = new XMLHttpRequest();
+  // xhr.addEventListener( "load", function(){
+  //     console.log( this );
+  // });
+  // xhr.addEventListener( "error", function(){
+  //     console.log( this );
+  // });
+  // xhr.open("POST", "http://reqres.in/api/posts", true);
+  // xhr.send("title=Foo&body=Bar&userId=100000000000000");
 
 $ = {
   ajax: function(options) {
@@ -26,6 +24,9 @@ $ = {
     if (options.complete) {
       xhr.addEventListener( "load", function() {
         options.complete(this, this.statusText);
+      });
+      xhr.addEventListener( "error", function() {
+        options.complete(this, this.statusText, this.status);
       });
     }
 
@@ -41,20 +42,56 @@ $ = {
       }
     }
 
-    if (options.method) {
-      xhr.open(options.method)
-    }
-
     if (options.success) {
-      xhr.addEventListener("success", function() {
-        options.succcess();
+      xhr.addEventListener("load", function() {
+        options.success(this.responseText, this.statusText, this);
       });
     }
 
-    if (options.data) {
-
+    if (options.dataType){
+      xhr.responseType = options.dataType;
+    }
+   
+    if (options.method) {
+      var method = options.method;
     }
 
+    if (options.url){
+      var url = options.url;
+    }
 
+    if (options.async){
+      var async = options.async;
+    }
+
+    xhr.open(method, url, async);
+
+    xhr.send(options.data); 
+  },
+
+  get: function(url, data, success, dataType){
+    var options = {method: "get",
+                   async: true,
+                   url: url,
+                   data: data,
+                   success: success,
+                   dataType: dataType
+                };
+     $.ajax(options);
   }
 }
+
+var successCallback = function(response){
+            console.log(response);
+          };
+
+$.get("http://reqres.in/api/users", null, successCallback, "text");
+
+// $.ajax({method: "get", 
+//           url: "http://reqres.in/api/users",
+//           async: true,
+//           success: function(response){
+//             console.log(response);
+//           }
+
+//           });
